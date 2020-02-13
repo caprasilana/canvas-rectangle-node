c2cd09244071213fed0f12284ebe5779c3251f11
acelano <caprasilana@gmail.com>
2020-02-13 11:57:44 +0100

    test loop on payload to fix canvas.toDataUrl()

--- node/canvas-rectangle-node.js
+++ node/canvas-rectangle-node.js
@@ -1,15 +1,11 @@
+module.exports = function (RED) {
 
+	function CanvasRectangleNode(config) {
+		RED.nodes.createNode(this, config);
+		var node = this;
+		const { createCanvas, loadImage } = require('canvas');
+		node.on('input', function (msg) {
 
-
-
-module.exports = function(RED) {
-
-    function CanvasRectangleNode(config) {
-        RED.nodes.createNode(this,config);
-        var node = this;
-const { createCanvas, loadImage } = require('canvas');
-        node.on('input', function(msg) {
-			
 			const canvas = createCanvas(200, 200);
 			const ctx = canvas.getContext('2d');
 
@@ -30,12 +26,28 @@ const { createCanvas, loadImage } = require('canvas');
 			// Draw cat with lime helmet
 			loadImage('/home/pi/.node-red/node_modules/node-red-dashboard/dist/CameraImages/frame.png').then((image) => {
 				ctx.drawImage(image, 50, 0, 70, 70);
-				msg.payload =  canvas.toDataURL();
-				
+
+				const imageBase64 = canvas.toDataURL();
+				const imgLength = imageBase64.length;
+				nOfLoop = parseInt(imgLength / 100) + (imgLength % 100 > 0 ? 1 : 0);
+
+				let payload = "";
+				for (let index = 0; index < nOfLoop; index++) {
+
+					maxIndex = (index * 100) + 100;
+					if (maxIndex > imgLength) {
+						maxIndex = imgLength;
+					}
+					payload = payload + imageBase64.substring(index * 100, maxIndex);
+
+				}
+
+				msg.payload = payload;
+
 			});
-		
-            node.send(msg);
-        });
-    }
-    RED.nodes.registerType("canvas-rectangle-node",CanvasRectangleNode);
+
+			node.send(msg);
+		});
+	}
+	RED.nodes.registerType("canvas-rectangle-node", CanvasRectangleNode);
 }
