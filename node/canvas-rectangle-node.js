b9f81ca02135778c7ed78e03a1bbaffb49d82995
acelano <caprasilana@gmail.com>
2020-02-15 21:33:07 +0100

    imagepath as param

--- node/canvas-rectangle-node.js
+++ node/canvas-rectangle-node.js
@@ -1,43 +1,55 @@
+// called when the runtime loads the node on startup
 module.exports = function (RED) {
 
+	// called whenever a new instance of the node is created
+	// the 'config' argument contains node specific properties set in the editor
 	function CanvasRectangleNode(config) {
 		RED.nodes.createNode(this, config);
-		var node = this;
+
+		// set properties
+		this.imagePath = config.imagePath
+		const node = this
+
 		const { createCanvas, loadImage } = require('canvas');
+
+		// register a listener to the 'input' event
+		// which gets called whenever a message arrives at the node
 		node.on('input', function (msg) {
 
-			
-
 			// Draw cat with lime helmet
-			loadImage('/home/pi/.node-red/node_modules/node-red-dashboard/dist/CameraImages/frame.png').then((image) => {
-			
-			var width = parseInt(image.width);
-			var height = parseInt(image.height);
-			const canvas = createCanvas(width,height);
-			const ctx = canvas.getContext('2d');
-			ctx.drawImage(image, 0, 0, width,height);
-			// Write "Hello World!"
-			ctx.font = '30px Impact';
-			ctx.rotate(0);
-			ctx.fillText('Hello World!', 50, 100);
+			loadImage(node.imagePath).then((image) => {
 
-			// Draw Basic Rectangle
-			ctx.beginPath();
-			ctx.rect(188, 50, 200, 100);
-			//context.fillStyle = 'yellow';
-			//context.fill();
-			ctx.lineWidth = 3;
-			ctx.strokeStyle = 'yellow';
-			ctx.stroke();
-				
+				var width = parseInt(image.width);
+				var height = parseInt(image.height);
+				const canvas = createCanvas(width, height);
+				const ctx = canvas.getContext('2d');
+				ctx.drawImage(image, 0, 0, width, height);
+
+				// Write "Hello World!"
+				ctx.font = '30px Impact';
+				ctx.rotate(0);
+				ctx.fillText('Hello World!', 50, 100);
+
+				// Draw Basic Rectangle
+				ctx.beginPath();
+				ctx.rect(188, 50, 200, 100);
+				//context.fillStyle = 'yellow';
+				//context.fill();
+				ctx.lineWidth = 3;
+				ctx.strokeStyle = 'yellow';
+				ctx.stroke();
+
 
 				msg.payload = canvas.toDataURL('image/png', 0.5).split('base64,')[1].toString();
 				node.send(msg);
 
 			});
 
-			
+
 		});
+
+		node.on('close', function () {
+		})
 	}
 	RED.nodes.registerType("canvas-rectangle-node", CanvasRectangleNode);
 }
